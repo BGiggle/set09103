1239386b9131ef39b7bc30b94f52d2033478b2c8
Brian Giggle <bgiggle@hotmail.co.uk>
2015-10-18 19:07:45 +0100

    Created Coursework1 folder

---
+++ Coursework1/static/app/controllers/collectionController.js
@@ -0,0 +1,6 @@
+﻿app.controller("collectionController", ["$scope", "dataService", function ($scope, dataService) {
+    dataService.getCollection(0, 10).$promise.then(function (result) {
+            $scope.collection = result.results;
+        });
+    }
+]);
