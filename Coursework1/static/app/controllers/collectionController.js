46c21067df0da3eaf4584a1136f27bde55f6216d
bgiggle <bgiggle@hotmail.co.uk>
2015-10-19 12:58:29 +0000

    added buildRequest

--- Coursework1/static/app/controllers/collectionController.js
+++ Coursework1/static/app/controllers/collectionController.js
@@ -1,4 +1,4 @@
-﻿app.controller("collectionController", ["$scope", "dataService", function ($scope, dataService) {
+app.controller("collectionController", ["$scope", "dataService", function ($scope, dataService) {
     dataService.getCollection(0, 10).$promise.then(function (result) {
             $scope.collection = result.results;
         });
--- Coursework1/testapp.py
+++ Coursework1/testapp.py
@@ -1,4 +1,4 @@
-﻿import requests
+import requests
 from flask import Flask, abort, url_for, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -11,10 +11,17 @@ def home():
 
 @app.route('/getCollection/', methods =['GET'])
 def about():
-    start =  request.args.get('start', '')
-    end =  request.args.get('end', '')
-    result  = { "result": [1,2,3,4,5,6,7,8,9,10]}       
-    return requests.get("http://www.giantbomb.com/api/games/?api_key=6ca5ed55e20277bf7ba7d998bac94b41c446e806&format=json&limit=10&field_list=name,deck,id,original_release_date,platforms").content
+    start =  int(request.args.get('start', '0'))
+    end =  int(request.args.get('end', '0'))
+    return requests.get(buildRequest(start,end)).content
+
+def buildRequest(start, end):
+    requestString = "http://www.giantbomb.com/api/games/?api_key=6ca5ed55e20277bf7ba7d998bac94b41c446e806&format=json&field_list=name,deck,id,original_release_date,platforms"  
+    requestString += "&limit=" + str(end - start)
+    requestString += "&offset=" + str(start)
+    return requestString
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
